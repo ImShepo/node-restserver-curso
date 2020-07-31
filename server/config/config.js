694226ca598d5c94fef99c7632fa6b45f0f78b32
Shepo <piperincon.1098@gmail.com>
2020-07-31 01:20:14 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,20 @@
 // Puerto
 // ====================================
 process.env.PORT = process.env.PORT || 3000;
+
+// ====================================
+// Entorno
+// ====================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ====================================
+// Base de datos
+// ====================================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://imshepo:UkTc7WzlWZX5FdeG@cluster0.7w7zw.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+process.env.URLDB = urlDB;
---
+++ server/routes/usuario.js
@@ -0,0 +1,126 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const Usuario = require('../models/usuario');
+const usuario = require('../models/usuario');
+const app = express();
+
+app.get('/usuario', function(req, res) {
+
+    // Solo trae registros cuyo estado está activo
+
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+    Usuario.find({ estado: true }, 'nombre email role estado google img')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+            Usuario.count({ estado: true }, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    usuarios,
+                    cuantos: conteo
+                });
+            });
+        });
+});
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+
+    console.log(body.password);
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
+    });
+
+    usuario.save((err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+
+    // if (body.nombre === undefined) {
+    //     res.status(400).json({
+    //         ok: false,
+    //         mensaje: 'El nombre es necesario'
+    //     });
+    // } else {
+    //     res.json({
+    //         persona: body
+    //     });
+    // }
+});
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+    Usuario.findOneAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioActualizado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioActualizado
+        });
+    });
+});
+
+app.delete('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+
+    // Usuario.findOneAndRemove(id, (err, usuarioBorrado) => {
+
+    // Ponemos el estado en false
+    let cambiaEstado = {
+        estado: false
+    }
+    Usuario.findOneAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
+
+        // Validamos el error SIEMPRE SE DEBE USAR
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        };
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no encontrado'
+                }
+            });
+        };
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        });
+    });
+});
+
+module.exports = app;
